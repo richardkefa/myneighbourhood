a2817b6ec6728b669e02a618c7be29f126a1c8bc
richardkefa <richardkefa@gmail.com>
2020-10-30 19:04:39 +0300

    add profile migrations snd env file

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import dj_database_url
 from decouple import config,Csv
 import cloudinary
 import cloudinary.uploader
@@ -24,12 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3h3pyrlzytjvdu39o=dw(ji0@i+5fm3w*jzaf-@gv9s9^ez84a'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -41,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
+    'users.apps.UsersConfig',
     'cloudinary',
 ]
 
