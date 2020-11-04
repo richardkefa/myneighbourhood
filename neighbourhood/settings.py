841485a139379a57bc871bf4f5b1438ed22988a8
richardkefa <richardkefa@gmail.com>
2020-11-04 09:16:38 +0300

    add import os on settings

--- neighbourhood/settings.py
+++ neighbourhood/settings.py
@@ -17,6 +17,7 @@ from decouple import config,Csv
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
